a73eeaf78c8de598670d57c812a390730d686bbe
Archie Warnock <warnock@awcubed.com>
2021-05-18 17:33:59 -0400

    Add OSDD retrieval and parsing

--- cmr_monitor.pl
+++ cmr_monitor.pl
@@ -9,6 +9,11 @@ use 5.010;
 use Config::Tiny;
 use Data::Dumper::Concise;
 use Getopt::Long;
+use LWP;
+use LWP::Protocol::https;
+use WWW::Mechanize::Timed;
+use XML::LibXML;
+use XML::LibXML::PrettyPrint;
 
 =pod
 
@@ -31,8 +36,13 @@ GetOptions(
     verbose => \$verbose,
     );
 
-my $inifile  = q{cmr.ini};
-my $config   = Config::Tiny->read( $inifile, 'utf8' );
+my $inifile = q{cmr.ini};
+my $config  = Config::Tiny->read( $inifile, 'utf8' );
+#my $browser = LWP::UserAgent->new();
+my $browser = WWW::Mechanize::Timed->new(
+    protocols_allowed => ['http', 'https'],
+    ssl_opts => { verify_hostname => 1 }
+    );
 
 {
     my $name;
@@ -42,6 +52,41 @@ my $config   = Config::Tiny->read( $inifile, 'utf8' );
         $name = $config->{$key}->{name};
         say "Got $name from config" if $name;
         $osdd = $config->{$key}->{osdd};
-        say "  - Got $osdd for $name" if $osdd;
+        if ($osdd) {
+            say "  - Got $osdd for $name";
+            my $osdd_status = get_osdd($osdd);
+            foreach $key (%$osdd_status) {
+                say "  - $key: " . $osdd_status->{$key}
+                if $osdd_status->{$key};
+            }
+        }
     }
+}
+
+sub get_osdd {
+    my $url = shift;
+    my %status;
+    my $response = $browser->get( $url );
+    $status{code}         = $response->status_line;
+    if ($response->is_success) {
+        $status{content_type} = $response->content_type;
+        $status{total_time}   = $browser->client_total_time;
+        $status{elapsed_time} = $browser->client_elapsed_time;
+        my $content = $response->content;
+        $content =~  s/^\s+//;
+        my $document;
+        eval {
+            $document = XML::LibXML->load_xml(string => $content);
+        };
+        if ($@) {
+            say "Failed to parse XML";
+        }
+        else {
+            my $pp = XML::LibXML::PrettyPrint->new(indent_string => "  ");
+            $pp->pretty_print($document); # modified in-place
+            say $document->toString;
+        }
+    }
+
+    return \%status;
 }
