62f15bd183bf6154ff28c56d8ffe40bcb4d5a65e
Archie Warnock <warnock@awcubed.com>
2021-05-24 14:36:18 -0400

    Update schema

--- sql/cmr_monitor.sql
+++ sql/cmr_monitor.sql
@@ -1,119 +1,88 @@
--- phpMyAdmin SQL Dump
--- version 4.9.5
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.19  Distrib 10.3.28-MariaDB, for Linux (x86_64)
 --
--- Host: localhost
--- Generation Time: May 19, 2021 at 10:21 PM
--- Server version: 10.3.28-MariaDB
--- PHP Version: 7.4.19
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: cmr_monitor
+-- ------------------------------------------------------
+-- Server version	10.3.28-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `cmr_monitor`
---
-
--- --------------------------------------------------------
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `monitor`
 --
 
+DROP TABLE IF EXISTS `monitor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `monitor` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `request_type` enum('osdd','granule') NOT NULL,
   `http_status` int(11) NOT NULL,
   `total_time` float DEFAULT NULL,
   `elapsed_time` float DEFAULT NULL,
   `http_message` varchar(16) DEFAULT NULL,
   `parsed` varchar(16) DEFAULT NULL,
   `error` varchar(128) DEFAULT NULL,
-  `timestamp` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
-  `fk_sourceid` int(11) NOT NULL
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp(),
+  `fk_source` varchar(10) NOT NULL,
+  `url` text NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_sourcename` (`fk_source`),
+  CONSTRAINT `fk_sourcename` FOREIGN KEY (`fk_source`) REFERENCES `source` (`source`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `monitor`
+--
+
+LOCK TABLES `monitor` WRITE;
+/*!40000 ALTER TABLE `monitor` DISABLE KEYS */;
+/*!40000 ALTER TABLE `monitor` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `source`
 --
 
+DROP TABLE IF EXISTS `source`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `source` (
-  `sourceid` int(11) NOT NULL,
-  `source` enum('CMR','CCMEO','EUMETSAT','GHRSST','ISRO','INPE','MOSDAC','USGS','CGEOSS','NRSCC') NOT NULL UNIQUE,
+  `sourceid` int(11) NOT NULL AUTO_INCREMENT,
+  `source` varchar(10) NOT NULL,
   `label` varchar(64) NOT NULL,
-  `status` enum('ACTIVE','INACTIVE','UNKNOWN','') NOT NULL DEFAULT 'ACTIVE'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `status` enum('ACTIVE','INACTIVE','UNKNOWN','') NOT NULL DEFAULT 'ACTIVE',
+  PRIMARY KEY (`sourceid`),
+  KEY `source` (`source`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `source`
 --
 
-INSERT INTO `source` (`sourceid`, `source`, `label`, `status`) VALUES
-(1, 'CMR', 'NASA/CMR (USA)', 'ACTIVE'),
-(2, 'CCMEO', 'CCMEO (Canada)', 'ACTIVE'),
-(3, 'EUMETSAT', 'EUMETSAT (ESA)', 'ACTIVE'),
-(4, 'GHRSST', 'NOAA-NCEI/GHRSST (USA)', 'ACTIVE'),
-(5, 'ISRO', 'ISRO/NRSC (India)', 'ACTIVE'),
-(6, 'INPE', 'INPE (Brazil)', 'INACTIVE'),
-(7, 'MOSDAC', 'ISRO/MOSDAC (India)', 'ACTIVE'),
-(8, 'NRSCC', 'NRSCC (China)', 'UNKNOWN'),
-(9, 'CGEOSS', 'GEOSS (China)', 'UNKNOWN'),
-(10, 'USGS', 'USGS/LSI', 'ACTIVE'),;
+LOCK TABLES `source` WRITE;
+/*!40000 ALTER TABLE `source` DISABLE KEYS */;
+INSERT INTO `source` VALUES (1,'CMR','NASA/CMR (USA)','ACTIVE'),(2,'CCMEO','CCMEO (Canada)','ACTIVE'),(3,'EUMETSAT','EUMETSAT (ESA)','ACTIVE'),(4,'GHRSST','NOAA-NCEI/GHRSST (USA)','ACTIVE'),(5,'ISRO','ISRO/NRSC (India)','ACTIVE'),(6,'INPE','INPE (Brazil)','INACTIVE'),(7,'MOSDAC','ISRO/MOSDAC (India)','ACTIVE'),(8,'NRSCC','NRSCC (China)','UNKNOWN'),(9,'CGEOSS','GEOSS (China)','UNKNOWN'),(10,'USGS','USGS/LSI','ACTIVE');
+/*!40000 ALTER TABLE `source` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `monitor`
---
-ALTER TABLE `monitor`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_sourceid` (`sourceid`);
-
---
--- Indexes for table `source`
---
-ALTER TABLE `source`
-  ADD PRIMARY KEY (`sourceid`),
-  ADD KEY `idx_source` (`source`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `monitor`
---
-ALTER TABLE `monitor`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `source`
---
-ALTER TABLE `source`
-  MODIFY `sourceid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `monitor`
---
-ALTER TABLE `monitor`
-  ADD CONSTRAINT `fk_sourceid` FOREIGN KEY (`sourceid`) REFERENCES `source` (`sourceid`);
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-05-24 14:36:08
