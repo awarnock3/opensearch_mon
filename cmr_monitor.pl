e8f4c27797adc03da5a992f54890e57f16afc4a5
Archie Warnock <warnock@awcubed.com>
2021-06-03 16:35:42 -0400

    Finished adding error alert email

--- cmr_monitor.pl
+++ cmr_monitor.pl
@@ -21,6 +21,12 @@ use Pod::Usage;
 use DBI;
 use Term::ReadKey;
 use File::Basename;
+use Net::SMTP 3.0;
+use Email::Sender::Transport::SMTP;
+use Email::Stuffer;
+use MIME::Types;
+use Authen::SASL qw(Perl);
+
 
 use Menu;
 
@@ -151,24 +157,24 @@ my $browser = WWW::Mechanize::Timed->new(
 my $dbname = $config->{database}->{dbname};
 my $dbuser = $config->{database}->{dbuser};
 my $dbpass = $config->{database}->{dbpass};
-my $dsn = qq{dbi:mysql:$dbname};
+my $dsn    = qq{dbi:mysql:$dbname};
 
 my $dbh = DBI->connect($dsn,$dbuser,$dbpass)
     or die "Couldn't connect to database: " . DBI->errstr;
 
 my $links = get_links_all();
 
- MAIN:
+MAIN:
 {
-    if ($batch) {
-        batch();
-    }
-    elsif ($source) {
-        get_single($source);
-    }
-    else {
-        menu();
-    }
+  if ($batch) {
+    batch();
+  }
+  elsif ($source) {
+    get_single($source);
+  }
+  else {
+    menu();
+  }
 }
 $dbh->disconnect;
 
@@ -179,30 +185,30 @@ Present the interative menu of sources
 =cut
 
 sub menu {
-    my $menu = Menu->new( );
-    $menu->title( q{Test CWIC Source} );
+  my $menu = Menu->new( );
+  $menu->title( q{Test CWIC Source} );
 
-    my $sources = get_active_sources();
-    foreach my $source (sort keys %$sources) {
-        $menu->add(
-            $sources->{$source}->{label} => sub{
-                get_single( $source );
-                pause();
-                $menu->print();
-            },
-        );
-    }
-
-    # Build the rest of the menu
+  my $sources = get_active_sources();
+  foreach my $source (sort keys %$sources) {
     $menu->add(
-        'Options'    => sub{ options_menu(); },
-        );
-    $menu->add(
-        'Exit'       => sub{ $menu->exit(1); },
-        );
+               $sources->{$source}->{label} => sub{
+                 get_single( $source );
+                 pause();
+                 $menu->print();
+               },
+              );
+  }
 
-    $menu->print();
-    return 0;
+  # Build the rest of the menu
+  $menu->add(
+             'Options'    => sub{ options_menu(); },
+            );
+  $menu->add(
+             'Exit'       => sub{ $menu->exit(1); },
+            );
+
+  $menu->print();
+  return 0;
 }
 
 =head2 options_menu()
@@ -212,49 +218,49 @@ Present the menu to set optional parameters
 =cut
 
 sub options_menu {
-    my $options = Menu->new( );
-    $options->title( q{Set parameters} );
+  my $options = Menu->new( );
+  $options->title( q{Set parameters} );
 
-    if ($save) {
-        $options->add(
-            'Set save off'      => sub{ $save = 0; },
-            );
-    }
-    else {
-        $options->add(
-            'Set save on'       => sub{ $save = 1; },
-            );
-    }
-    if ($verbose) {
-        $options->add(
-            'Set verbose off'   => sub{ $verbose = 0; },
-            );
-    }
-    else {
-        $options->add(
-            'Set verbose on'    => sub{ $verbose = 1; },
-            );
-    }
-    unless ($osdd_only) {
-        $options->add(
-            'Get OSDD only'     => sub{
-                $osdd_only    = 1;
-                $granule_only = 0;
-            },
-            );
-    }
-    unless ($granule_only) {
-        $options->add(
-            'Get granules only' => sub{
-                $granule_only = 1;
-                $osdd_only    = 0;
-            },
-            );
-    }
-    $options->add( 'Return'     => sub{ menu();} );
-    $options->print();
-    menu();
-    return 0;
+  if ($save) {
+    $options->add(
+                  'Set save off'      => sub{ $save = 0; },
+                 );
+  }
+  else {
+    $options->add(
+                  'Set save on'       => sub{ $save = 1; },
+                 );
+  }
+  if ($verbose) {
+    $options->add(
+                  'Set verbose off'   => sub{ $verbose = 0; },
+                 );
+  }
+  else {
+    $options->add(
+                  'Set verbose on'    => sub{ $verbose = 1; },
+                 );
+  }
+  unless ($osdd_only) {
+    $options->add(
+                  'Get OSDD only'     => sub{
+                    $osdd_only    = 1;
+                    $granule_only = 0;
+                  },
+                 );
+  }
+  unless ($granule_only) {
+    $options->add(
+                  'Get granules only' => sub{
+                    $granule_only = 1;
+                    $osdd_only    = 0;
+                  },
+                 );
+  }
+  $options->add( 'Return'     => sub{ menu();} );
+  $options->print();
+  menu();
+  return 0;
 }
 
 =head2 batch()
@@ -264,45 +270,63 @@ Run in batch mode across all sources
 =cut
 
 sub batch {
-    my $name;
+  my $name;
 
-    foreach my $key (sort keys %$links) {
-        $name = $links->{uc $key}->{fk_source};
-        next unless is_active($name);
+  foreach my $key (sort keys %$links) {
+    $name = $links->{uc $key}->{fk_source};
+    next unless is_active($name);
 
-        say "Got $name from config" if ($name and $verbose);
-        unless ($granule_only) {
-            my $osdd_link = $links->{uc $key}->{osdd};
-            if ($osdd_link) {
-                say "  - Got $osdd_link for $name" if $verbose;
-                my $get_status = get_osdd($osdd_link);
-                $get_status->{source} = uc $key;
-                if ($verbose) {
-                    foreach my $param (%$get_status) {
-                        say "  - $param: " . $get_status->{$param}
-                        if ($get_status->{$param});
-                    }
-                }
-                db_save($get_status) if $save;
-            }
+    say "Got $name from config" if ($name and $verbose);
+    unless ($granule_only) {
+      my $osdd_link = $links->{uc $key}->{osdd};
+      if ($osdd_link) {
+        say "  - Got $osdd_link for $name" if $verbose;
+        my $get_status = get_osdd($osdd_link);
+        $get_status->{source} = uc $key;
+        if (defined $get_status->{error}) {
+          my $source  = $get_status->{source};
+          my $type    = $get_status->{request_type};
+          my $subject = qq{CWIC Monitor Alert};
+          $subject   .= qq{: $source $type};
+          my $msg     = qq{Error retrieving OSDD:\n};
+          $msg       .= Dumper( $get_status );
+          mail_alert($subject, $msg);
         }
-        unless ($osdd_only) {
-            my $granule_link = $links->{uc $key}->{granule};
-            if ($granule_link) {
-                say "  - Got $granule_link for $name" if $verbose;
-                my $get_status = get_granules($granule_link);
-                $get_status->{source} = uc $key;
-		if ($verbose) {
-		    foreach my $key (sort keys %$get_status) {
-			say "  - $key: " . $get_status->{$key}
-			if $get_status->{$key};
-		    }
-		}
-                db_save($get_status) if $save;
-            }
+        if ($verbose) {
+          foreach my $param (%$get_status) {
+            say "  - $param: " . $get_status->{$param}
+              if ($get_status->{$param});
+          }
         }
+        db_save($get_status) if $save;
+      }
     }
-    return 0;
+    unless ($osdd_only) {
+      my $granule_link = $links->{uc $key}->{granule};
+      if ($granule_link) {
+        say "  - Got $granule_link for $name" if $verbose;
+        my $get_status = get_granules($granule_link);
+        $get_status->{source} = uc $key;
+        if (defined $get_status->{error}) {
+          my $source  = $get_status->{source};
+          my $type    = $get_status->{request_type};
+          my $subject = qq{CWIC Monitor Alert};
+          $subject   .= qq{: $source $type};
+          my $msg     = qq{Error retrieving OSDD:\n};
+          $msg       .= Dumper( $get_status );
+          mail_alert($subject, $msg);
+        }
+        if ($verbose) {
+          foreach my $key (sort keys %$get_status) {
+            say "  - $key: " . $get_status->{$key}
+              if $get_status->{$key};
+          }
+        }
+        db_save($get_status) if $save;
+      }
+    }
+  }
+  return 0;
 }
 
 =head2 get_single()
@@ -312,45 +336,45 @@ Test one specified source
 =cut
 
 sub get_single {
-    my $target = shift;
-    return unless is_active($target);
-    
-    my $name;
+  my $target = shift;
+  return unless is_active($target);
 
-    if ($target and length $target > 0) {
-        $name = $links->{uc $target}->{fk_source};
-        say "Got $name from config" if $name;
-        unless ($granule_only) {
-            my $osdd_link = $links->{uc $target}->{osdd};
-            if ($osdd_link) {
-                say "  - Got $osdd_link for $name";
-                my $get_status = get_osdd($osdd_link);
-                $get_status->{source} = uc $target;
-                foreach my $key (sort keys %$get_status) {
-                    say "  - $key: " . $get_status->{$key}
-                    if $get_status->{$key};
-                }
-                db_save($get_status) if $save;
-            }
+  my $name;
+
+  if ($target and length $target > 0) {
+    $name = $links->{uc $target}->{fk_source};
+    say "Got $name from config" if $name;
+    unless ($granule_only) {
+      my $osdd_link = $links->{uc $target}->{osdd};
+      if ($osdd_link) {
+        say "  - Got $osdd_link for $name";
+        my $get_status = get_osdd($osdd_link);
+        $get_status->{source} = uc $target;
+        foreach my $key (sort keys %$get_status) {
+          say "  - $key: " . $get_status->{$key}
+            if $get_status->{$key};
         }
-        unless ($osdd_only) {
-            my $granule_link = $links->{uc $target}->{granule};
-            if ($granule_link) {
-                say "  - Got $granule_link for $name";
-                my $get_status = get_granules($granule_link);
-                $get_status->{source} = uc $target;
-                foreach my $key (sort keys %$get_status) {
-                    say "  - $key: " . $get_status->{$key}
-                    if $get_status->{$key};
-                }
-                db_save($get_status) if $save;
-            }
+        db_save($get_status) if $save;
+      }
+    }
+    unless ($osdd_only) {
+      my $granule_link = $links->{uc $target}->{granule};
+      if ($granule_link) {
+        say "  - Got $granule_link for $name";
+        my $get_status = get_granules($granule_link);
+        $get_status->{source} = uc $target;
+        foreach my $key (sort keys %$get_status) {
+          say "  - $key: " . $get_status->{$key}
+            if $get_status->{$key};
         }
+        db_save($get_status) if $save;
+      }
     }
-    else {
-        say "No source specified";
-    }
-    return 0;
+  }
+  else {
+    say "No source specified";
+  }
+  return 0;
 }
 
 =head2 get_osdd($url)
@@ -360,10 +384,10 @@ Retrieve the OSDD response from the remote source
 =cut
 
 sub get_osdd {
-    my $url    = shift;
-    my $status = process($url);
-    $status->{request_type} = 'osdd';
-    return $status;
+  my $url    = shift;
+  my $status = process($url);
+  $status->{request_type} = 'osdd';
+  return $status;
 }
 
 =head2 get_granules($url)
@@ -373,10 +397,10 @@ Retrieve the granule response from the remote source
 =cut
 
 sub get_granules {
-    my $url    = shift;
-    my $status = process($url);
-    $status->{request_type} = 'granule';
-    return $status;
+  my $url    = shift;
+  my $status = process($url);
+  $status->{request_type} = 'granule';
+  return $status;
 }
 
 =head2 process($url)
@@ -387,48 +411,47 @@ the status hash.
 =cut
 
 sub process {
-    my $url = shift;
-    my %status;
-    
-    my $response     = $browser->get( $url );
-    $status{url}     = $url;
-    $status{code}    = $response->code;
-    $status{message} = $response->status_line;
-    if ($response->is_success) {
-        $status{total_time}   = $browser->client_total_time;
-        $status{elapsed_time} = $browser->client_elapsed_time;
-        if ($status{code} == 200) {
-            my $content = $response->decoded_content;
-            my $document;
-            eval {
-                $document = XML::LibXML->new->load_xml(string => $content);
-            };
-            if ($@) {
-                $status{parsed} = "failed";
-                #                $status{error}  = $@;
-                my $error = $@;
-                $status{error} = $error->{code} . " " . $error->{message};
-                say "Failed to parse XML: $status{error}" if $verbose;
-            }
-            else {
-                $status{parsed} = "success";
-                if ($verbose) {
-                    my $pp = XML::LibXML::PrettyPrint->new(indent_string => "  ");
-                    $pp->pretty_print($document); # modified in-place
-                    say $document->toString;
-                }
-            }
+  my $url = shift;
+  my %status;
+
+  my $response     = $browser->get( $url );
+  $status{url}     = $url;
+  $status{code}    = $response->code;
+  $status{message} = $response->status_line;
+  if ($response->is_success) {
+    $status{total_time}   = $browser->client_total_time;
+    $status{elapsed_time} = $browser->client_elapsed_time;
+    if ($status{code} == 200) {
+      my $content = $response->decoded_content;
+      my $document;
+      eval {
+        $document = XML::LibXML->new->load_xml(string => $content);
+      };
+      if ($@) {
+        $status{parsed} = "failed";
+        my $error = $@;
+        $status{error} = $error->{code} . " " . $error->{message};
+        say "Failed to parse XML: $status{error}" if $verbose;
+      }
+      else {
+        $status{parsed} = "success";
+        if ($verbose) {
+          my $pp = XML::LibXML::PrettyPrint->new(indent_string => "  ");
+          $pp->pretty_print($document); # modified in-place
+          say $document->toString;
         }
-        else {
-            $status{parsed} = "failed";
-            $status{error}  = "HTTP GET " . $status{code};
-        }
+      }
     }
     else {
-        $status{parsed} = "failed";
-        $status{error}  = "HTTP GET failed";
+      $status{parsed} = "failed";
+      $status{error}  = "HTTP GET " . $status{code};
     }
-    return \%status;
+  }
+  else {
+    $status{parsed} = "failed";
+    $status{error}  = "HTTP GET failed";
+  }
+  return \%status;
 }
 
 =head2 db_save($status_hashptr)
@@ -438,27 +461,27 @@ Persist the status hash into the database
 =cut
 
 sub db_save {
-    my $status = shift;
+  my $status = shift;
 
-    my $sql = q{INSERT INTO monitor 
-                       (id, request_type, http_status, total_time,
-                        elapsed_time, http_message, parsed, fk_source, url)
-                 VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?)};
-    eval {
-        $dbh->do($sql, {}, $status->{request_type}, $status->{code},
-                 $status->{total_time}, $status->{elapsed_time},
-                 $status->{message}, $status->{parsed}, $status->{source},
-                 $status->{url} );
-    };
-    if ($@) {
-        die "Insert failed";
-    }
-    if (defined $status->{parsed} and $status->{parsed} eq 'failed') {
-        my $monitor_id = $dbh->{mysql_insertid};
-        $dbh->do(q{UPDATE monitor SET error = ? WHERE id = ?},
-                 {}, $status->{error}, $monitor_id);
-    }
-    return 0;
+  my $sql = q{INSERT INTO monitor
+                     (id, request_type, http_status, total_time,
+                      elapsed_time, http_message, parsed, fk_source, url)
+              VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?)};
+  eval {
+    $dbh->do($sql, {}, $status->{request_type}, $status->{code},
+             $status->{total_time}, $status->{elapsed_time},
+             $status->{message}, $status->{parsed}, $status->{source},
+             $status->{url} );
+  };
+  if ($@) {
+    die "Insert failed";
+  }
+  if (defined $status->{parsed} and $status->{parsed} eq 'failed') {
+    my $monitor_id = $dbh->{mysql_insertid};
+    $dbh->do(q{UPDATE monitor SET error = ? WHERE id = ?},
+             {}, $status->{error}, $monitor_id);
+  }
+  return 0;
 }
 
 =head2 get_links_all
@@ -468,9 +491,9 @@ Grab the OSDD and Granule request links from the database
 =cut
 
 sub get_links_all {
-    my $sql   = q{SELECT fk_source,osdd,granule from links};
-    my $all_links = $dbh->selectall_hashref($sql, 'fk_source');
-    return $all_links;
+  my $sql       = q{SELECT fk_source,osdd,granule from links};
+  my $all_links = $dbh->selectall_hashref($sql, 'fk_source');
+  return $all_links;
 }
 
 =head2 is_active
@@ -480,10 +503,10 @@ Return 1 if the source is activef
 =cut
 
 sub is_active {
-    my $test_source = shift;
-    my $sql = q{SELECT status FROM source where source = ?};
-    my $active = $dbh->selectrow_arrayref($sql, {}, $test_source);
-    return ($active->[0] eq 'ACTIVE') ? 1 : 0;
+  my $test_source = shift;
+  my $sql         = q{SELECT status FROM source where source = ?};
+  my $active      = $dbh->selectrow_arrayref($sql, {}, $test_source);
+  return ($active->[0] eq 'ACTIVE') ? 1 : 0;
 }
 
 =head2 get_active_sources()
@@ -493,9 +516,9 @@ Return list of all active sources and labels from Source table
 =cut
 
 sub get_active_sources {
-    my $sql = q{SELECT source,label FROM source WHERE status = 'ACTIVE'};
-    my $sources = $dbh->selectall_hashref($sql, 'source');
-    return $sources;
+  my $sql     = q{SELECT source,label FROM source WHERE status = 'ACTIVE'};
+  my $sources = $dbh->selectall_hashref($sql, 'source');
+  return $sources;
 }
 
 =head2 pause()
@@ -505,12 +528,52 @@ Wait for a keypress to continue
 =cut
 
 sub pause() {
-    print "Press any key to continue...";
+  print "Press any key to continue...";
 
-    ReadMode 'cbreak';
-    ReadKey(0);
-    ReadMode 'normal';
-    return 0;
+  ReadMode 'cbreak';
+  ReadKey(0);
+  ReadMode 'normal';
+  return 0;
+}
+
+=pod
+
+=head2 mail_alert($fromname, $response)
+
+Emails an alert to someone
+
+=cut
+
+sub mail_alert {
+  my $subject    = shift;
+  my $profile    = shift;
+
+  my $sender     = $config->{mail}->{sender};
+  my $user       = $config->{mail}->{login};
+  my $password   = $config->{mail}->{password};
+  my $recipients = $config->{mail}->{recipients};
+  my @receivers  = split ",",$recipients;
+
+  #print "Sending to $receivers";
+  my $transport = Email::Sender::Transport::SMTP->new(
+                    {
+                     host          => 'mail.runbox.com',
+                     ssl           => 1,
+                     sasl_username => $user,
+                     sasl_password => $password
+                    }
+                                                     );
+
+    # print "Got MimeType $mime_type for $saved_file";
+  my $msg = Email::Stuffer
+    ->from     ($sender   )
+    ->to       (@receivers)
+    ->text_body($profile  )
+    ->subject  ($subject  )
+    ->transport($transport)
+    ;
+
+  $msg->send;
 }
 
 # ABSTRACT: Monitor/test CWIC OpenSearch sources
